a0a134ff6e83eb866453aac91361e49285351532
Brandon Mellus <mellusbrandon@gmail.com>
2021-10-28 16:38:45 -0700

    Create User model

---
+++ server/models/User.js
@@ -0,0 +1,55 @@
+const mongoose, { Schema, SchemaTypes } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: /\w+@\w+\.\w{2,3}/
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 8,
+        maxLength: 20
+    },
+    userRatings: [{
+        type: SchemaTypes.ObjectId,
+        ref: 'UserRating'
+    }]
+},
+{
+    toJSON: {
+        virtuals: true
+    }
+});
+
+UserSchema.pre('init', async function savePassword(next) {
+    this.password = await bcrypt.hash(this.password, 10);
+    this.save();
+    next();
+});
+
+UserSchema.methods.checkPassword = async function checkPassword(password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+UserSchema.methods.addUserRating = function addUserRating(userRating) {
+    this.userRatings.push(userRating);
+    this.save();
+}
+
+UserSchema.virtual('averageRating').get(function averageRating() {
+    return this.userRatings.reduce(
+        (total, rating) => total + rating.value
+    ) / this.userRatings.length;
+});
+
+const User = mongoose.model(User, 'User');
+module.exports = User;
