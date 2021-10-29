62ab873c18ef3ebd976a269c4aa6f320d0f38df8
Brandon Mellus <mellusbrandon@gmail.com>
2021-10-28 17:33:35 -0700

    Create Category model

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,11 @@
 const User = require('./User');
+const UserRating = require('./UserRating');
+const Class = require('./Class');
+const Category = require('./Category');
 
 module.exports = {
     User,
     UserRating,
-    Class
+    Class,
+    Category
 }
