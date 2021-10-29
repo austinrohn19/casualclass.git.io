76aeac93e57bb5bf28012009608e11e0ea4b5b56
Brandon Mellus <mellusbrandon@gmail.com>
2021-10-28 17:58:30 -0700

    Create Review model

--- server/models/index.js
+++ server/models/index.js
@@ -2,10 +2,12 @@ const User = require('./User');
 const UserRating = require('./UserRating');
 const Class = require('./Class');
 const Category = require('./Category');
+const Review = require('./Review');
 
 module.exports = {
     User,
     UserRating,
     Class,
-    Category
+    Category,
+    Review
 }
