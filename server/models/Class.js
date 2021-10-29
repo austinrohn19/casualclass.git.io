92637f3f65dcd229086661ac567175688527af8a
Brandon Mellus <mellusbrandon@gmail.com>
2021-10-28 18:13:33 -0700

    Add author field to Class model

--- server/models/Class.js
+++ server/models/Class.js
@@ -5,6 +5,10 @@ const ClassSchema = new Schema({
         type: String,
         required: true
     },
+    author: {
+        type: SchemaTypes.ObjectId,
+        ref: 'User'
+    },
     description: {
         type: String,
         required: true
