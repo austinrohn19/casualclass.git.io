0ecc790b643f2fc801d5390b911af6a5eb445aaa
harrisonarmstrong93@gmail.com <harrisonarmstrong93@gmail.com>
2021-10-30 13:42:23 -0400

    Added @apollo/client dependency and added Apollo Provider to App.js with Auth support

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,58 @@
+//Import Needed Modules
 import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+//Import Components and Pages
 import Header from './components/Header'
 import homePage from './pages/homePage';
 import Footer from './components/Footer';
 
+//Import Semantic UI CSS for all components and pages to use
 import 'semantic-ui-css/semantic.min.css'
+
+const httpLink = createHttpLink({
+  uri: '/graphql'
+})
+
+// Create request middleware that will attach the JWT to every request as an authorization header
+const authLink = setContext((_, { headers }) => {
+  //get the token from local storage is it exists
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+//Set up the Apollo Client
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
 
 function App() {
   return (
-    <Router>
-      <>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={homePage} />
-        </Switch>
-        <Footer />
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Header />
+          <Switch>
+            <Route exact path='/' component={homePage} />
+          </Switch>
+          <Footer />
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
