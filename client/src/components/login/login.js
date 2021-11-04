ad0d7310c5cf3793e76d87b19b54cc52c582609b
harrisonarmstrong93@gmail.com <harrisonarmstrong93@gmail.com>
2021-11-04 18:49:23 -0400

    Login Page functioning

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,8 +1,40 @@
 /* eslint-disable no-useless-constructor */
-import React from "react";
+import React, { useState } from "react";
 import "./style.scss";
 
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../../utils/mutations';
+import Auth from '../../utils/auth';
+
 const Login = (props) => {
+
+  const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+  const [loginUser, {error, data }] = useMutation(LOGIN_USER);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  }
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await loginUser({
+        variables: {...userFormData },
+      })
+
+      Auth.login(data.login.token)
+
+    } catch (error) {
+      console.log(error);
+    }
+
+    setUserFormData({
+      username: '',
+      password: ''
+    })
+  }
 
   return (
     <div className="base-container" ref={props.containerRef}>
@@ -11,21 +43,19 @@ const Login = (props) => {
         <div className="image">
           <img src="../login.png" alt="" />
         </div>
-        <div className="form">
+        <form className="form" onSubmit={handleFormSubmit}>
           <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input type="text" name="username" placeholder="username" />
+            <label htmlFor="email">E-Mail</label>
+            <input type="text" name="email" placeholder="E-Mail" onChange={handleInputChange}/>
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" placeholder="password" />
+            <input type="password" name="password" placeholder="Password" onChange={handleInputChange}/>
           </div>
-        </div>
-      </div>
-      <div className="footer">
-        <button type="button" className="btn">
-          Login
-        </button>
+          <button type="submit" className="btn">
+            Login
+          </button>
+        </form>
       </div>
     </div>
   );
