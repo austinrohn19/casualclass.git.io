ebccf134112ae8e7bfa6585803820aa7176d5e2c
harrisonarmstrong93@gmail.com <harrisonarmstrong93@gmail.com>
2021-11-04 20:39:10 -0400

    Created a query on front end for all of the server side queries

---
+++ client/src/utils/queries.js
@@ -0,0 +1,87 @@
+import { gql } from '@apollo/client'
+
+export const QUERY_CLASSES = gql`
+    query classes {
+        class {
+            title
+            author {
+                username
+                email
+            }
+            description
+            previewVideoUrl
+            category {
+                name
+            }
+            reviews {
+                author
+                rating
+                text
+                createdOn
+            }
+            createdOn
+            popularity
+            cost
+        }
+    }
+`
+
+export const QUERY_CLASS = gql`
+    query class($_id: ID!) {
+        class(_id: $_id) {
+            title
+            author {
+                username
+                email
+            }
+            description
+            previewVideoUrl
+            category {
+                name
+            }
+            reviews {
+                author
+                rating
+                text
+                createdOn
+            }
+            createdOn
+            popularity
+            cost
+        }
+    }
+`
+
+export const QUERY_CATEGORIES = gql`
+    query categories {
+        category {
+            name
+        }
+    }
+`
+
+export const QUERY_USER = gql`
+    query user($_id: ID!) {
+        user(_id: $_id) {
+            username
+            email
+            userRatings
+            createdClasses
+            averageRating
+        }
+    }
+`
+
+export const QUERY_ME = gql`
+    query me {
+        username 
+        email
+        userRatings
+        createdClasses
+        averageRating
+    }
+`
+
+
+
+
