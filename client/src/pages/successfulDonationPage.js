86b30e0046df5bfb6b871ee207a059a7c02d983a
Brandon Mellus <mellusbrandon@gmail.com>
2021-11-05 20:55:37 -0700

    Remove some unnecessary lines

--- client/src/pages/successfulDonationPage.js
+++ client/src/pages/successfulDonationPage.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
 import { Header } from 'semantic-ui-react';
-import queryString from 'query-string';
 
 function SuccessfulDonationPage() {
-    const { search } = useLocation();
-    const { session_id } = queryString.parse(search);
     return (
         <div className="successful-donation-page">
             <Header as="h2">Success!</Header>
